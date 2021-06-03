34847912a6cb60be202e142ea9d8e699ac0c4374
antonia <tsm.antonia@gmail.com>
2021-06-03 11:23:52 +0200

    actualizando notificacion

--- app/src/main/java/com/toni/notifyme/MainActivity.java
+++ app/src/main/java/com/toni/notifyme/MainActivity.java
@@ -5,18 +5,31 @@ import androidx.core.app.NotificationCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.QuickContactBadge;
 
 public class MainActivity extends AppCompatActivity {
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private static final String ACTION_UPDATE_NOTIFICATION = "com.toni.notifyme.ACTION_UPDATE_NOTIFICATION";
+
     private NotificationManager mNotifyManager;
     private static final int NOTIFICATION_ID = 0;
+
     Button btn1;
     Button btn2;
     Button btn3;
+
+    private NotificationReceiver mReceiver = new NotificationReceiver();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,19 +38,44 @@ public class MainActivity extends AppCompatActivity {
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
-        
+        setNotificationButtonState(true, false, false);
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //crear notificacion
                 enviarNotificacion();
             }
         });
 
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //actualizar notificacion
+                updateNotification();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //cancelar notificacion
+                cancelNotification();
+            }
+        });
+
         createNotificationChannel();
+
+        registerReceiver(mReceiver, new IntentFilter(ACTION_UPDATE_NOTIFICATION));
     }
 
     private void enviarNotificacion() {
+        Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast(this, NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        setNotificationButtonState(false, true, true);
         NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        notifyBuilder.addAction(R.drawable.ic_update, "Update Notification", updatePendingIntent);
         mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 
@@ -60,10 +98,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private NotificationCompat.Builder getNotificationBuilder(){
+        //crea un pending intent para que cuando se seleccione la notificación se abra la MainActivity
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Bitmap androidImage = BitmapFactory.decodeResource(getResources(), R.drawable.mascot_1);
         NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
                 .setContentTitle("Has sido notificado!")
                 .setContentText("Este es el texto de tu notificación.")
-                .setSmallIcon(R.drawable.ic_stat_colors);
+                .setSmallIcon(R.drawable.ic_stat_colors)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
         return notifyBuilder;
+    }
+
+    public void updateNotification(){
+        setNotificationButtonState(false, false, true);
+        Bitmap androidImage = BitmapFactory.decodeResource(getResources(), R.drawable.mascot_1);
+        NotificationCompat.Builder notifiyBuilder = getNotificationBuilder();
+        notifiyBuilder.setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(androidImage)
+                .setBigContentTitle("Notificación actualizada"));
+        mNotifyManager.notify(NOTIFICATION_ID, notifiyBuilder.build());
+    }
+
+    public void cancelNotification(){
+        setNotificationButtonState(true, false, false);
+        mNotifyManager.cancel(NOTIFICATION_ID);
+    }
+
+    private void setNotificationButtonState(Boolean isNotifyEnabled, Boolean isUpdateEnabled, Boolean isCancelEnabled){
+        btn1.setEnabled(isNotifyEnabled);
+        btn2.setEnabled(isUpdateEnabled);
+        btn3.setEnabled(isCancelEnabled);
+    }
+
+    public class NotificationReceiver extends BroadcastReceiver{
+        public NotificationReceiver(){
+
+        }
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateNotification();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
